12849b378aa668ff41e25dab0c8c82d730a131d0
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-06 14:40:57 +0300

    Print the configuration value to console

--- ExcelReader/Program.cs
+++ ExcelReader/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+IConfigurationBuilder builder = new ConfigurationBuilder();
+
+var currentDirectory = Directory.GetCurrentDirectory();
+builder.SetBasePath(currentDirectory);
+builder.AddJsonFile("appsettings.json");
+
+IConfiguration config = builder.Build();
+
+var ePPlus = config.GetSection("EPPlus");
+var excelPackage = ePPlus.GetSection("ExcelPackage");
+var licenseContext = excelPackage.GetSection("LicenseContext");
+
+Console.WriteLine($"{licenseContext.Value}");
