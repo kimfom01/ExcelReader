6c2a1a722855e1966a9283f37e67aa45fc5952f8
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-07 17:56:55 +0300

    Update backup script

--- ExcelReader/SqlScripts/SetupDbScript (copy).sql
+++ ExcelReader/SqlScripts/SetupDbScript (copy).sql
@@ -1,27 +1,26 @@
-DROP DATABASE IF EXISTS ExcelDb
-BEGIN
-  CREATE DATABASE ExcelDb
-END
+USE master
 GO
-
+DROP DATABASE IF EXISTS ExcelDb
+GO
+CREATE DATABASE ExcelDb
+GO
 USE ExcelDb
 GO
-
 BEGIN
   CREATE TABLE Products
   (
     ProductId INT,
-    ProductName NVARCHAR,
-    Supplier NVARCHAR,
+    ProductName NVARCHAR(100),
+    Supplier NVARCHAR
+(100),
     ProductCost FLOAT
   );
 END
 GO
-
 CREATE PROCEDURE dbo.Products_Insert
   @ProductId INT,
-  @ProductName NVARCHAR,
-  @Supplier NVARCHAR,
+  @ProductName NVARCHAR(100),
+  @Supplier NVARCHAR(100),
   @ProductCost FLOAT
 AS
 BEGIN
@@ -37,10 +36,9 @@ BEGIN
     (
       @ProductId, @ProductName,
       @Supplier, @ProductCost
-        )
+    )
 END
 GO
-
 CREATE PROCEDURE dbo.Products_Select
 AS
 BEGIN
