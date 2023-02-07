eb7dc450ee666855f44497ecfd7f1f17e5976b8a
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-07 04:42:30 +0300

    Add sql scripts to setup the database

---
+++ ExcelReader/SqlScripts/SetupDbScript (copy).sql
@@ -0,0 +1,50 @@
+DROP DATABASE IF EXISTS ExcelDb
+BEGIN
+  CREATE DATABASE ExcelDb
+END
+GO
+
+USE ExcelDb
+GO
+
+BEGIN
+  CREATE TABLE Products
+  (
+    ProductId INT,
+    ProductName NVARCHAR,
+    Supplier NVARCHAR,
+    ProductCost FLOAT
+  );
+END
+GO
+
+CREATE PROCEDURE dbo.Products_Insert
+  @ProductId INT,
+  @ProductName NVARCHAR,
+  @Supplier NVARCHAR,
+  @ProductCost FLOAT
+AS
+BEGIN
+  SET NOCOUNT ON
+  INSERT INTO Products
+    (
+    ProductId,
+    ProductName,
+    Supplier,
+    ProductCost
+    )
+  VALUES
+    (
+      @ProductId, @ProductName,
+      @Supplier, @ProductCost
+        )
+END
+GO
+
+CREATE PROCEDURE dbo.Products_Select
+AS
+BEGIN
+  SELECT *
+  FROM Products
+END
+GO
---
+++ ExcelReader/SqlScripts/SetupDbScript.sql
@@ -0,0 +1,47 @@
+USE master
+GO
+DROP DATABASE IF EXISTS ExcelDb
+GO
+CREATE DATABASE ExcelDb
+GO
+USE ExcelDb
+GO
+BEGIN
+  CREATE TABLE Products
+  (
+    ProductId INT,
+    ProductName NVARCHAR,
+    Supplier NVARCHAR,
+    ProductCost FLOAT
+  );
+END
+GO
+CREATE PROCEDURE dbo.Products_Insert
+  @ProductId INT,
+  @ProductName NVARCHAR,
+  @Supplier NVARCHAR,
+  @ProductCost FLOAT
+AS
+BEGIN
+  SET NOCOUNT ON
+  INSERT INTO Products
+    (
+    ProductId,
+    ProductName,
+    Supplier,
+    ProductCost
+    )
+  VALUES
+    (
+      @ProductId, @ProductName,
+      @Supplier, @ProductCost
+        )
+END
+GO
+CREATE PROCEDURE dbo.Products_Select
+AS
+BEGIN
+  SELECT *
+  FROM Products
+END
+GO
