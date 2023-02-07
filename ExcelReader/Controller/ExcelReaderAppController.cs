4be68ec40e5dff2ad288b85d31ac29f1b7563a3b
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-07 17:53:50 +0300

    Call the new db setup method

--- ExcelReader/Controller/ExcelReaderAppController.cs
+++ ExcelReader/Controller/ExcelReaderAppController.cs
@@ -8,19 +8,25 @@ namespace ExcelReader.Controller;
 public class ExcelReaderAppController : IReaderAppController
 {
     private readonly IDataAccess _dataAccess;
+    private readonly ISetupDatabase _setupDatabase;
     private readonly IReaderService _readerService;
     private readonly ITableVisualization _tableVisualization;
 
-    public ExcelReaderAppController(IDataAccess dataAccess, IReaderService readerService, ITableVisualization tableVisualization)
+    public ExcelReaderAppController(IDataAccess dataAccess,
+                                        ISetupDatabase setupDatabase,
+                                        IReaderService readerService,
+                                        ITableVisualization tableVisualization
+                                    )
     {
         _dataAccess = dataAccess;
+        _setupDatabase = setupDatabase;
         _readerService = readerService;
         _tableVisualization = tableVisualization;
     }
 
     public async Task RunProgram()
     {
-        _dataAccess.SetupDatabase();
+        SetupDatabase();
 
         var productsFromExcel = await ReadFromExcelAsync();
 
@@ -34,18 +40,25 @@ public class ExcelReaderAppController : IReaderAppController
         Thread.Sleep(3000);
     }
 
+    private void SetupDatabase()
+    {
+        Console.WriteLine("Setting up database...");
+        _setupDatabase.Setup();
+        Console.WriteLine("Done\n");
+    }
+
     private void DisplayDataOnConsole(List<Product> productsFromDb)
     {
         Console.WriteLine("Preparing to display data from database...");
         _tableVisualization.DisplayProductsTable(productsFromDb);
-        Console.WriteLine("Done");
+        Console.WriteLine("Done\n");
     }
 
     private async Task<List<Product>> ReadFromDatabaseAsync()
     {
         Console.WriteLine("Reading from database...");
         var productsFromDb = await _dataAccess.GetProductsAsync();
-        Console.WriteLine("Done");
+        Console.WriteLine("Done\n");
         return productsFromDb;
     }
 
@@ -53,14 +66,14 @@ public class ExcelReaderAppController : IReaderAppController
     {
         Console.WriteLine("Saving data to the database...");
         await _dataAccess.AddProducts(productsFromExcel);
-        Console.WriteLine("Done");
+        Console.WriteLine("Done\n");
     }
 
     private async Task<List<Product>> ReadFromExcelAsync()
     {
         Console.WriteLine("Reading from the excel file...");
         var productsFromExcel = await _readerService.LoadProductsFromExcelAsync();
-        Console.WriteLine("Done");
+        Console.WriteLine("Done\n");
 
         return productsFromExcel;
     }
