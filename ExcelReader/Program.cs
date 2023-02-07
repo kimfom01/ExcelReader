2519c08c73b6235d4846351fe6aa3a5efd818797
Kim Fom <57032138+kimfom01@users.noreply.github.com>
2023-02-07 04:44:48 +0300

    Build the container and call the entry method

--- ExcelReader/Program.cs
+++ ExcelReader/Program.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using ExcelReader.ConsoleTableViewer;
+using ExcelReader.Controller;
+using ExcelReader.Data;
+using ExcelReader.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 IConfigurationBuilder builder = new ConfigurationBuilder();
 
@@ -8,8 +13,20 @@ builder.AddJsonFile("appsettings.json");
 
 IConfiguration config = builder.Build();
 
-var ePPlus = config.GetSection("EPPlus");
-var excelPackage = ePPlus.GetSection("ExcelPackage");
-var licenseContext = excelPackage.GetSection("LicenseContext");
+// var ePPlus = config.GetSection("EPPlus");
+// var excelPackage = ePPlus.GetSection("ExcelPackage");
+// var licenseContext = excelPackage.GetSection("LicenseContext");
 
-Console.WriteLine($"{licenseContext.Value}");
+// Console.WriteLine($"{licenseContext.Value}");
+
+var services = new ServiceCollection();
+services.AddSingleton(config);
+services.AddTransient<IDataAccess, DapperDataAccess>();
+services.AddTransient<IReaderService, EPPlusReaderService>();
+services.AddTransient<ITableVisualization, ConsoleTableBuilderVisualization>();
+services.AddTransient<IReaderAppController, ExcelReaderAppController>();
+
+var serviceProvider = services.BuildServiceProvider();
+var startup = serviceProvider.GetService<IReaderAppController>();
+
+await startup!.RunProgram();
